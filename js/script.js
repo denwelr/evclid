e2f2e8e1df6b4ebe75305d77bf92920c0ae6ba6e
denwelr <85834107+denwelr@users.noreply.github.com>
2021-10-31 19:15:24 +0400

    added script for btn to scroll to top

--- js/script.js
+++ js/script.js
@@ -25,6 +25,7 @@ searchBtn.addEventListener('click', event => {
     searchForm.classList.add('header__search-form_focus');
     searchInput.focus();
 });
+
 searchCloseBtn.addEventListener('click', () => {
     searchForm.classList.remove('header__search-form_focus');
 });
@@ -34,6 +35,8 @@ document.addEventListener('mousedown', event => {
     if(event.target.tagName === 'A' || event.target.tagName === 'BUTTON') event.preventDefault();
 });
 
+
+
 /* header-slider */
 $('.header__slider').slick({
     arrows: false,
@@ -42,6 +45,8 @@ $('.header__slider').slick({
     autoplay: true,
     autoplaySpeed: 7000,
 });
+
+
 
 /* burger menu */
 
@@ -95,13 +100,6 @@ const tabsPlugin = () => {
 }
 tabsPlugin();
 
-// ADAPTIVE
-
-if(window.innerWidth <= 320) {
-    const previewBlocks = document.querySelectorAll('.about__advantages-item_preview .about__advantages-item-body');
-
-    previewBlocks.forEach(block => block.parentElement.after(block));
-} 
 
 // SPOILERS
  
@@ -120,17 +118,16 @@ window.addEventListener('resize', event => {
     if(window.innerWidth <= 320) {
         const previewBody = document.querySelectorAll('.about__advantages-item_preview .about__advantages-item-body');
 
-        previewBody.forEach(body => {
+        previewBody.forEach(body => { 
             body.parentElement.after(body);
         });
     }
 });
 
 /* ПЛАВНЫЙ СКРОЛЛ */
-
-document.querySelector('.header__nav').addEventListener('click', function(event){
-    const target = event.target.closest('.header__link');
-    if(target === null) return;
+document.body.addEventListener('click', function(event){
+    const target = event.target;
+    if(target.tagName !== 'A') return;
 
     const targetElement = document.getElementById(target.getAttribute('href').slice(1));
     
@@ -142,3 +139,31 @@ document.querySelector('.header__nav').addEventListener('click', function(event)
         behavior: 'smooth',
     })
 });
+/* /ПЛАВНЫЙ СКРОЛЛ */
+
+/* КНОПКА НАВЕРХ */
+
+const upBtn = document.querySelector('.up-btn');
+
+const scrollHeight = Math.max(
+    document.body.scrollHeight, document.documentElement.scrollHeight,
+    document.body.offsetHeight, document.documentElement.offsetHeight,
+    document.body.clientHeight, document.documentElement.clientHeight
+);
+
+window.addEventListener('scroll', function(){
+    if(scrollY > (scrollHeight - 1000) && !upBtn.classList.contains('up-btn_active')) {
+        upBtn.classList.add('up-btn_active')
+    } 
+    else if(scrollY < (scrollHeight - 1000) && upBtn.classList.contains('up-btn_active')) {
+        upBtn.classList.remove('up-btn_active')
+    }
+});
+upBtn.addEventListener('click', event => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+});
+
+/* /КНОПКА НАВЕРХ */
